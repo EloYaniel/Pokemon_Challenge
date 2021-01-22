fa50a6c6411c110b0b1ae29b75e863d7b0e96ad4
EloYaniel <73134002+EloYaniel@users.noreply.github.com>
2021-01-21 23:38:39 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { PokerCard } from "./components";
+import "./styles.css";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <PokerCard />
+    </div>
+  );
+};
